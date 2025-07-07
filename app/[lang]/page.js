650803924ff454c098dd8fa4a8122e85e827768e
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-07-07 23:09:54 +0600

    test: Internalization with bn and en

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { getDictionary } from "./dictionaries";
 
-export default function Home() {
+export default async function Home({ params: { lang } }) {
+  const dictionaries = await getDictionary(lang);
   return (
     <div className="container">
       Welcome to PhotoFeed
+      <h1>{dictionaries.followers}....</h1>
     </div>
   );
 }
